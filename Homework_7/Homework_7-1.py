18869517268fd282c44abe6a4e2ff1ea6a731ae8
Fabian Otto <fabi.otto@t-online.de>
2017-12-05 22:06:57 +0100

    finished exercise 7

--- Homework_7/Homework_7-1.py
+++ Homework_7/Homework_7-1.py
@@ -3,23 +3,26 @@ from nltk.corpus import brown
 from collections import Counter
 
 
-corpus = brown.words()
-tagged_corpus = nltk.pos_tag(corpus)
-tagged_corpus_universal = nltk.pos_tag(corpus, tagset="universal")
+tagged_corpus = brown.tagged_words()
+tagged_corpus_universal = brown.tagged_words(tagset="universal")
 tagged_corpus_set = set(tagged_corpus)
 
 # a)
-a = [word for (word, tag) in tagged_corpus_set if tag == 'MD']
-a = sorted(a)
+# Capitalized words are intentionally included
+# Daniil said we have to analyse them separately
+# Anyway, here with only lower case words as well:
+a = sorted(set([word.lower() for (word, tag) in tagged_corpus_set if tag == 'MD']))
+# a = sorted([word for (word, tag) in tagged_corpus_set if tag == 'MD'])
 
 # b)
 # This list comprehension reduces the result
 # to all singular third person verbs (VBZ)
-# and all plural common nouns (NNS), which end in -s/-es
+# and all plural common nouns (NNS).
+# Using a set for b avoids duplicates of "word : NNS/VBZ"
+# This reduces the next step to finding duplicates.
 b = [word for (word, tag) in tagged_corpus_set if tag == 'VBZ' or tag == 'NNS']
 
-# get duplicates, means matches of "word : [VBZ, NNS]"
-# Using a set for b avoids matches like "word : [NNS, NNS]", etc.
+# return the duplicates, means matches of "word : [VBZ, NNS]"
 b = [word for word, count in Counter(b).items() if count > 1]
 
 # c)
@@ -29,18 +32,24 @@ indices = [index for index, (_, tag) in enumerate(tagged_corpus_universal) if ta
 
 c = set()
 for index in indices:
+    # check if following two tokens are DET and NOUN
     det = tagged_corpus_universal[index+1]
     noun = tagged_corpus_universal[index+2]
     if det[1] != 'DET' or noun[1] != 'NOUN':
         continue
+    # if yes add all three words to result
     c.add((tagged_corpus_universal[index][0], det[0], noun[0]))
 
 # d)
-# The Brown corpus only provides English text.
-# This means Pronouns do not have gender, unlike German, French, Spanish, etc.
-# Therefore, the ratio is 0, 1 or undefined.
-# Depending on your point of view.
+feminine = {"she", "her", "herself", "hers"}
+masculine = {"he", "his", "himself", "him"}
+
+# pretty much just check if PRON matches on element in the above sets
+# I have not found a matching POS tag for this
+masculine_count = len([word for (word, tag) in tagged_corpus_universal if tag == "PRON" and word.lower() in feminine])
+feminine_count = len([word for (word, tag) in tagged_corpus_universal if tag == "PRON" and word.lower() in masculine])
 
 print("Distinct words with 'MD' POS tag in alphabetical order: ", a)
 print("VBZ and NNS with same spelling: ", b)
 print("ADP + DET + NN combinations : ", c)
+print("Ratio of masculine to feminine pronouns: ", masculine_count / feminine_count)
--- Homework_7/Homework_7-2.py
+++ Homework_7/Homework_7-2.py
@@ -3,23 +3,80 @@ from nltk.corpus import brown
 from texttable import Texttable
 
 
-corpus = brown.words()
-tagged_corpus_universal = nltk.pos_tag(corpus, tagset="universal")
+tagged_corpus_universal = brown.tagged_words(tagset="universal")
+corpus_length = len(tagged_corpus_universal)
 tagged_corpus_universal_set = set(tagged_corpus_universal)
 
-# create dict from keys [1, 10] in order to get at least 0
-# when no word with that amount was found
-count_dict = nltk.defaultdict(int).fromkeys(range(1, 11), 0)
+# container for best elems
+most_tagged_words = set()
+max_len = 1
+count_dict = nltk.defaultdict(int)
+
 for word, tags in nltk.Index(tagged_corpus_universal_set).items():
-    # get amount of tags and ensure it is not more than 10, increment counter by one
+    # get amount of tags increment counter by one
+    # save word to most_tagged if
     ctr = len(tags)
-    if ctr > 10:
-        continue
+
+    if ctr > max_len:
+        max_len = ctr
+        most_tagged_words = set()
+
+    if ctr == max_len:
+        most_tagged_words.add(word)
+
     count_dict[ctr] += 1
 
 # this package makes printing tables easier
 t = Texttable()
-t.add_row(['Number of POS Tags', 'Number of Words'])
-for k, v in count_dict.items():
-    t.add_row([k, v])
-print(t.draw())
+t.header(['Number of POS Tags', 'Number of Words'])
+# iterate over keys [1, 10] in order to get at least 0
+# when no word with that amount of tags was found
+for i in range(1, 11):
+    t.add_row([i, count_dict[i]])
+print(t.draw() + "\n")
+
+sentences = nltk.defaultdict(list)
+for word in most_tagged_words:
+    checked_tags = set()
+
+    for i, item in enumerate(tagged_corpus_universal):
+
+        temp_word = item[0]
+        tag = item[1]
+
+        if temp_word != word or tag in checked_tags:
+            continue
+
+        temp_sentence = [word]
+
+        # get word until previous punctuation
+        iter_index = i-1
+        while not (tagged_corpus_universal[iter_index][1] == "." and tagged_corpus_universal[iter_index][0] == '.'):
+            temp_sentence = [tagged_corpus_universal[iter_index][0]] + temp_sentence
+            iter_index -= 1
+            if iter_index < 0:
+                break
+
+        # get word until next punctuation
+        iter_index = i+1
+        while not (tagged_corpus_universal[iter_index][1] == "." and tagged_corpus_universal[iter_index][0] == '.'):
+            temp_sentence.append(tagged_corpus_universal[iter_index][0])
+            iter_index += 1
+            if iter_index > corpus_length:
+                break
+
+        sentences[word].append(tag + ": " + " ".join(temp_sentence) + ".")
+        checked_tags.add(tag)
+
+        if len(checked_tags) == max_len:
+            break
+
+print("Usage of words with most tags of ", max_len)
+t = Texttable()
+t.header(['Word', 'Sentences'])
+t.set_cols_width([10, 500])
+# iterate over keys [1, 10] in order to get at least 0
+# when no word with that amount of tags was found
+for word, sents in sentences.items():
+    t.add_row([word + "\n"*max_len, "\n".join(sents)])
+print(t.draw() + "\n")
